8f5ba81568a766743cdd287fefbaadcff8223d72
Schultz Alexis <developer@alexisschultz.fr>
2017-08-09 19:25:04 +0200

    - Added tests for PokemonDetailsInteractor save and unsave functions - Added code coverage to test target

--- PokedexTests/PokedexTests.swift
+++ PokedexTests/PokedexTests.swift
@@ -10,27 +10,84 @@ import XCTest
 @testable import Pokedex
 
 class PokedexTests: XCTestCase {
+    var pokemonDetailsInteractor :PokemonDetailsInteractor!
+    var pokemon : Pokemon!
+    var pokemonId : Int!
+    var pokemonImageUrl : String!
+    var pokemonName : String!
+    var pokemonWeight : Int!
+    var pokemonHeight : Int!
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        pokemonId = 999
+        pokemonImageUrl = "http://dummyUrl"
+        pokemonName = "testpokemon"
+        pokemonWeight = 66
+        pokemonHeight = 99
+        pokemonDetailsInteractor = PokemonDetailsInteractor()
+        pokemon = Pokemon()
+        pokemon.id = pokemonId
+        pokemon.name = pokemonName
+        pokemon.imageUrl = pokemonImageUrl
+        pokemon.height = pokemonHeight
+        pokemon.weight = pokemonWeight
+        cleanUpDB()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        pokemonDetailsInteractor = nil
+        pokemon = nil
+        pokemonId = nil
+        pokemonImageUrl = nil
+        pokemonName = nil
+        pokemonWeight = nil
+        pokemonHeight = nil
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testSaveGoodPokemon() throws
+    {
+        try pokemonDetailsInteractor.save(pokemon)
+        {
+            self.expectation(description: "Pokemon saved").fulfill()
+        }
+
+        waitForExpectations(timeout: 2, handler: nil)
+
+        if let pokemonTest = DatabaseProvider.shared.getPokemon(with: pokemonName)
+        {
+            XCTAssert(pokemonTest.id == pokemon.id)
+            XCTAssert(pokemonTest.name == pokemon.name)
+            XCTAssert(pokemonTest.imageUrl == pokemon.imageUrl)
+            XCTAssert(pokemonTest.height == pokemon.height)
+            XCTAssert(pokemonTest.weight == pokemon.weight)
+        }
+        else
+        {
+            XCTFail("Failed to save pokemon")
         }
     }
-    
+
+    func testUnsavePokemon() throws
+    {
+        try DatabaseProvider.shared.insert(pokemon)
+
+        try pokemonDetailsInteractor.unsave(pokemon)
+        {
+            self.expectation(description: "Pokemon unsaved").fulfill()
+        }
+
+        waitForExpectations(timeout: 2, handler: nil)
+
+        XCTAssertNil(DatabaseProvider.shared.getPokemon(with: pokemonName))
+    }
+
+    func cleanUpDB()
+    {
+        if let pokemonTest = DatabaseProvider.shared.getPokemon(with: pokemonName)
+        {
+            try! DatabaseProvider.shared.delete(pokemonTest)
+        }
+    }
 }
